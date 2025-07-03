91dc54925644f873dd27cc1705dfaaa774cc5bf0
Carter <speers.carter@gmail.com>
2025-07-03 17:06:02 -0400

    chore: added metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A personal blog about my experiences and beliefs in tech',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://carters-blog.vercel.app/',
+  siteRepo: 'https://github.com/Carter907/se-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
