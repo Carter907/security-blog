ba2312dd39137e4ca28283e6be684afffcc1ca17
Carter <speers.carter@gmail.com>
2026-06-02 21:00:48 -0400

    chore: updated project thumbnails

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Welcome to the projects I've done!
+           Here are some notable projects I've worked on. I'll be adding more soon. 
           </p>
         </div>
         <div className="container py-12">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,15 +8,14 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'A Keylogger made in C++',
-    description: `Ever wanted to keep track of your key strokes on Linux? Well, now you can with
-    this simple keylogger made in C++. It requires sudo privileges to run, and is very accurate and fast.`,
+    description: `Here is a keylogger for Linux systems made for simplicity and speed.`,
     imgSrc: '/static/images/key.png',
     href: 'https://github.com/Carter907/keylogger',
   },
   {
     title: 'A Java Build Tool',
-    description: `Are you sick of using Maven or Gradle? Well, me too-- I made this simple build tool for Java projects that allows you to
-    build, run, and package your java projects with ease.`,
+    description: `Streamline is a simple build tool for Java projects aimed at helping students interact and run Java programmers
+		without any of the friction that's inheriently present in enterprise build systems.`,
     imgSrc: '/static/images/java-build-tool.png',
     href: 'https://github.com/Carter907/streamline',
   },
